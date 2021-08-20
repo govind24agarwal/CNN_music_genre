b878c784ce5c98ff88d51863cb06e321e7de86eb
Govind <govindagarwal24@gmail.com>
2021-08-20 23:14:04 +0530

    prepare_datasets method added, Loads data and splits it into train, test and validation sets.

--- music_genre.py
+++ music_genre.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 DATA_PATH = "/home/govind/Documents/ML/Velario Youtube/extracting_mfccs_music_genre/data.json"
 
@@ -46,6 +47,38 @@ def plot_history(history):
     axs[1].set_ylabel("Loss")
     axs[1].legend(loc="lower right")
     axs[1].set_title("loss eval")
+
+
+def prepare_datasets(test_size=0.25, validation_size=0.2):
+    """Loads data and splits it into train, test and validation sets
+
+    Args:
+        test_size (float, optional): Value in [0,1] indicating ratio of dataset to be used as test set. Defaults to 0.25.
+        validation_size (float, optional): Value in [0,1].indicating ratio of rest of the dataset to be used as validation set Defaults to 0.2.
+    Returns:
+        x_train (ndarray): Inputs used for training
+        x_validation (ndarray): Inputs used for validation
+        x_test (ndarray): Inputs used for testing
+        y_train (ndarray): Targets used for training
+        y_validation (ndarray): Targets used for validation
+        y_test (ndarray): Targets used for testing
+    """
+
+    # load data
+    x, y = load_data(DATA_PATH)
+
+    # create train, test and validation split
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=test_size)
+    x_train, x_validation, y_train, y_validation = train_test_split(
+        x_train, y_train, test_size=validation_size)
+
+    # add an axis to input sets
+    x_train = x_train[..., np.newaxis]
+    x_test = x_test[..., np.newaxis]
+    x_validation = x_validation[..., np.newaxis]
+
+    return x_train, x_test, x_validation, y_train, y_test, y_validation
 
 
 if __name__ == "__main__":
